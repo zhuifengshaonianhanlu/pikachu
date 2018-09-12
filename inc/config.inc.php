d98711da4d1ef0be282f8d53ec6d35f1dea10688
hanlu <317096829@qq.com>
2018-09-12 22:10:33 +0800

    Update config.inc.php

--- inc/config.inc.php
+++ inc/config.inc.php
@@ -8,7 +8,7 @@ header('Content-type:text/html;charset=utf-8');
 //定义数据库连接参数
 define('DBHOST', 'localhost');//将localhost修改为数据库服务器的地址
 define('DBUSER', 'root');//将root修改为连接mysql的用户名
-define('DBPW', 'root@root');//将root修改为连接mysql的密码
+define('DBPW', 'root');//将root修改为连接mysql的密码，如果改了还是连接不上，请先手动连接下你的数据库，确保数据库服务没问题在说！
 define('DBNAME', 'pikachu');//自定义，建议不修改
 define('DBPORT', '3306');//将3306修改为mysql的连接端口，默认tcp3306
 
