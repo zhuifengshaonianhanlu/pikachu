b34c1d3297971cdcfc8aa54aad0073e6ae0eaadb
hanlu <317096829@qq.com>
2018-10-30 16:33:04 +0800

    Update function.php
    update 服务端验证码不显示的问题

--- inc/function.php
+++ inc/function.php
@@ -25,6 +25,45 @@ function vcode($width=120,$height=40,$fontSize=30,$countElement=5,$countPixel=10
 	return $string;
 }
 
+//之前的验证码有点问题，重新从网上搜了一个简单的验证码函数，是的，从网上搜的。
+function vcodex(){
+	
+	$string = "abcdefghijklmnopqrstuvwxyz0123456789";
+    	$str = "";
+    	for($i=0;$i<6;$i++){
+        	$pos = rand(0,35);
+        	$str .= $string{$pos};
+    	}
+    //session_start();
+    //$_SESSION['img_number'] = $str;
+	
+	$img_handle = Imagecreate(80, 20);  //图片大小80X20
+    	$back_color = ImageColorAllocate($img_handle, 255, 255, 255); //背景颜色（白色）
+    	$txt_color = ImageColorAllocate($img_handle, 0,0, 0);  //文本颜色（黑色）
+    
+    //加入干扰线
+    	for($i=0;$i<3;$i++)
+    	{
+        	$line = ImageColorAllocate($img_handle,rand(0,255),rand(0,255),rand(0,255));
+        	Imageline($img_handle, rand(0,15), rand(0,15), rand(100,150),rand(10,50), $line);
+    	}
+    //加入干扰象素
+    	for($i=0;$i<200;$i++) 
+    	{
+        	$randcolor = ImageColorallocate($img_handle,rand(0,255),rand(0,255),rand(0,255));
+        	Imagesetpixel($img_handle, rand()%100 , rand()%50 , $randcolor);
+    	}
+	
+	Imagefill($img_handle, 0, 0, $back_color);             //填充图片背景色
+    	ImageString($img_handle, 28, 10, 0, $str, $txt_color);//水平填充一行字符串
+	
+	ob_clean();   // ob_clean()清空输出缓存区    
+    	header("Content-type: image/png"); //生成验证码图片    
+    	Imagepng($img_handle);//显示图片
+	return $str;
+	
+}
+
 
 //生成一个token,以当前微妙时间+一个5位的前缀
 function set_token(){
