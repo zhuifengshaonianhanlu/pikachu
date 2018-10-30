686c9111b3512d2aec3bccecd82c3c4d6d46fc3e
hanlu <317096829@qq.com>
2018-10-30 16:33:57 +0800

    Update showvcode.php
    update 服务端验证码不显示的问题

--- inc/showvcode.php
+++ inc/showvcode.php
@@ -1,7 +1,8 @@
 <?php 
 session_start();
 include_once 'function.php';
-$_SESSION['vcode']=vcode(100,40,30,4);
+//$_SESSION['vcode']=vcode(100,40,30,4);
+$_SESSION['vcode']=vcodex();
 //验证码绕过 on server 这里其实还是有一个问题,就是服务端将验证码字符串以明文COOKIE的方式给了前端,那验证码还有什么鸟意义。。。
 setcookie('bf[vcode]',$_SESSION['vcode']);
 ?>
